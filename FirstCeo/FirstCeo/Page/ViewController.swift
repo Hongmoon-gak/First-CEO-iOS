560fe964a62a069581a5b64fcddf9bb8e0876a01
lenamin <lenaminn@gmail.com>
2023-07-16 18:21:23 +0900

    [CHORE] set project

---
+++ FirstCeo/FirstCeo/Page/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstCeo
+//
+//  Created by Lena on 2023/07/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
