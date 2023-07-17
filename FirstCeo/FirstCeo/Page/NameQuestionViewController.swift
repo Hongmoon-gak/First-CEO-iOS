d72403da4d3734c6084c45f5f1a8ca59cfd9d92f
lenamin <lenaminn@gmail.com>
2023-07-17 12:16:25 +0900

    [FEAT] create name question view (#1)

---
+++ FirstCeo/FirstCeo/Page/NameQuestionViewController.swift
@@ -0,0 +1,18 @@
+//
+//  NameQuestionViewController.swift
+//  FirstCeo
+//
+//  Created by Lena on 2023/07/17.
+//
+
+import UIKit
+
+class NameQuestionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        // Do any additional setup after loading the view.
+    }
+
+
