1672f5d41edca6d763cd72fc6c2a32cc4799c2d4
lenamin <lenaminn@gmail.com>
2023-07-20 20:21:31 +0900

    [CHORE] add literal

---
+++ FirstCeo/FirstCeo/Global/Literal.swift
@@ -0,0 +1,11 @@
+//
+//  Literal.swift
+//  FirstCeo
+//
+//  Created by Lena on 2023/07/20.
+//
+
+import UIKit
+
+let screenHeight = UIScreen.main.bounds.height
+let screenWidth = UIScreen.main.bounds.width
