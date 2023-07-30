bbf08a3cfe7d9412931d1fac08572fa253601709
lenamin <lenaminn@gmail.com>
2023-07-30 12:27:43 +0900

    [ADD] Law Model 수정

--- FirstCeo/FirstCeo/Network/Model/Law.swift
+++ FirstCeo/FirstCeo/Network/Model/Law.swift
@@ -9,14 +9,15 @@ import Foundation
 
 struct Law {
     var name: String
+    var imageName: String
     // TODO: 로직 생성할 때 필요한 프로퍼티 추가하기
 }
 
 extension Law {
     static let laws = [
-        Law(name: "4대보험"),
-        Law(name: "근로계약"),
-        Law(name: "근로환경"),
-        Law(name: "임금")
+        Law(name: "4대보험", imageName: "insurance.png"),
+        Law(name: "근로계약", imageName: "contract.png" ),
+        Law(name: "급여", imageName: "pay.png"),
+        Law(name: "임금", imageName: "fire.png")
     ]
 }
