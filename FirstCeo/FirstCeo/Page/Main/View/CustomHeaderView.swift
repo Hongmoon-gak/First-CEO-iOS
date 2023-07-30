a7b55cb121090ce028a995ffb1ad0e4657c1a75c
lenamin <lenaminn@gmail.com>
2023-07-30 12:27:31 +0900

    [ADD] HeaderView 구현

---
+++ FirstCeo/FirstCeo/Page/Main/View/CustomHeaderView.swift
@@ -0,0 +1,26 @@
+//
+//  CustomHeaderView.swift
+//  FirstCeo
+//
+//  Created by Lena on 2023/07/29.
+//
+
+import UIKit
+
+class CustomHeaderView: UICollectionReusableView {
+        
+    static let reuseIdentifier = "CustomHeaderView"
+    
+    private var titleLabel = UILabel()
+    
+    func configureLayout() {
+        addSubview(titleLabel)
+        titleLabel.setLabel(labelText: "관련 법령 보기", backgroundColor: .clear, weight: .semibold, textSize: 18, labelColor: .black)
+        titleLabel.anchor(top: self.topAnchor, leading: self.leadingAnchor, paddingTop: 29, paddingLeading: 20)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+//        titleLabel.frame = bounds
+    }
+}
