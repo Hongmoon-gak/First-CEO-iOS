85b1e461eeb63f9b2d1ef87d9a68a3e95ecb9777
lenamin <lenaminn@gmail.com>
2023-08-01 13:34:15 +0900

    [ADD] image 재사용 위한 extension 추가 (#6)

---
+++ FirstCeo/FirstCeo/Extension/Image+Extension.swift
@@ -0,0 +1,16 @@
+//
+//  Image+Extension.swift
+//  FirstCeo
+//
+//  Created by Lena on 2023/07/31.
+//
+
+import UIKit
+
+extension UIImageView {
+    
+    func setImage(named: String) {
+        self.image = UIImage(named: named)
+    }
+    
+}
